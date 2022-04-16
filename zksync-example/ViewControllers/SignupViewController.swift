40fe4ebd8c8e9da4dbdd8ba7e504375e2887a0d9
igibliss00@gmail.com <igibliss00@gmail.com>
2022-04-15 23:50:17 -0400

    Create SignupVC.

---
+++ zksync-example/ViewControllers/SignupViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SignupViewController.swift
+//  zksync-example
+//
+//  Created by J on 2022-04-15.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
